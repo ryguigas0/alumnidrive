c580c915a886331330193fb8659eb3e83c14c1ae
Guilherme Oliveira Casagrande <32659090+ryguigas0@users.noreply.github.com>
2021-02-25 09:44:11 -0300

    Recieving username, still missing user password

--- backend/server.go
+++ backend/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,8 +14,17 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendFile("../frontend/html/login.html", false)
 	})
+
 	app.Get("/arquivos", func(c *fiber.Ctx) error {
 		return c.SendFile("../frontend/html/arquivos.html")
+	})
+
+	app.Post("/login", func(c *fiber.Ctx) error {
+
+		log.Output(0, c.FormValue("username"))
+		//log.Output(0, c.FormValue("password"))
+
+		return c.Redirect("/arquivos")
 	})
 
 	app.Listen(":3000")
